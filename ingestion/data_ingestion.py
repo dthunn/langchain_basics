63de9dca72ecd47741de6c53fe25f74fea568d07
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-01-11 16:31:35 -0700

    fixed ingestion formatting

--- ingestion/data_ingestion.py
+++ ingestion/data_ingestion.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader, PyPDFLoader, WebBaseLoader, ArxivLoader, WikipediaLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter, RecursiveJsonSplitter
-import json
 import requests
 
 load_dotenv()
@@ -9,7 +8,7 @@ load_dotenv()
 loader = TextLoader("speech.txt")
 text_documents = loader.load()
 
-loader=WebBaseLoader(web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",))
+loader = WebBaseLoader(web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",))
 web_documents = loader.load()
 
 docs = ArxivLoader(query="1706.03762", load_max_docs=2).load()
@@ -28,11 +27,12 @@ print(final_documents)
 
 speech=""
 with open("speech.txt") as f:
-    speech=f.read()
+    speech = f.read()
 
 
-text_splitter=RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=20)
-text=text_splitter.create_documents([speech])
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=20)
+text = text_splitter.create_documents([speech])
+
 print(text)
 
 json_data = requests.get("https://api.smith.langchain.com/openapi.json").json()
