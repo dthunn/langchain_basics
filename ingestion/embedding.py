5e93782db1cee8a635d06bb2a2db44b3695e50f3
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-01-11 16:54:22 -0700

    embeddings cleanup

--- ingestion/embedding.py
+++ ingestion/embedding.py
@@ -9,25 +9,24 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
-# embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
-# embeddings=(
-#     OllamaEmbeddings(model="gemma:2b")  ##by default it ues llama2
-# )
+embeddings = (
+    OllamaEmbeddings(model="gemma:2b")  ##by default it ues llama2
+)
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
+loader = TextLoader('speech.txt')
+docs = loader.load()
 
-# loader=TextLoader('speech.txt')
-# docs=loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+final_documents = text_splitter.split_documents(docs)
 
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
-# final_documents = text_splitter.split_documents(docs)
+db = Chroma.from_documents(final_documents, embeddings)
 
-# db=Chroma.from_documents(final_documents, embeddings)
-
-# query = "It will be all the easier for us to conduct ourselves as belligerents"
-# retrieved_results = db.similarity_search(query)
+query = "It will be all the easier for us to conduct ourselves as belligerents"
+retrieved_results = db.similarity_search(query)
 
 text = "this is latest documents"
 query_result = embeddings.embed_query(text)
